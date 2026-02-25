858e03e41f26bd2cd170a0029fe08218ce8f4520
Rohan Kumar Jha <153646946+kumarrohan47@users.noreply.github.com>
2026-02-25 09:25:33 +0530

    Add files via upload

---
+++ springboot_PEP-main/src/main/java/com/example/CRUD/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.example.CRUD.controller;
+
+
+import com.example.CRUD.dto.UserDto;
+import com.example.CRUD.dto.UserResponseDto;
+import com.example.CRUD.entity.User;
+import com.example.CRUD.repository.UserRepository;
+import com.example.CRUD.service.UserService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("")
+    public List<UserResponseDto> getAllUsers(){
+      return userService.getUsers()   ;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserDto userDto){
+        try{
+            return ResponseEntity.ok(userService.createUser(userDto));
+        } catch (MessagingException e) {
+
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto getUser(@PathVariable long id){
+        return userService.getuser(id) ;
+    }
+
+    @PutMapping("")
+    public UserResponseDto UpdateUser(@RequestBody UserDto userDto){
+        return userService.updateUserPartial(userDto) ;
+    }
+
+    @DeleteMapping("/{id}")
+    public UserResponseDto deleteUser(@PathVariable long id){
+        return userService.deleteUser(id) ;
+    }
+
+
+    @GetMapping("/visible")
+    public ResponseEntity<String> toggleVisible(Authentication authentication){
+        String username = authentication.getName();
+        userService.toggleVisible(username);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+
+
+
+
+
+
+
+}
